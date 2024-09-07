11f62f0429fac196b7a7b31868c124a80fab613e
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-09-07 16:12:57 +0000

    Type Conversion

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,39 @@
+let score = "33";
+
+// console.log(typeof score);
+
+let scoreInNumber = Number(score);
+
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+// "33" -> 33
+// "" -> 0
+// "israfil" -> NaN
+// null -> 0
+// true -> 1
+
+
+
+let isLoggedIn = 0;
+
+// console.log(typeof isLoggedIn);
+
+let isLoggedInBool = Boolean(isLoggedIn);
+
+// console.log(isLoggedInBool);
+
+// "something" -> true
+// "" -> false
+// 0 -> false
+
+
+
+let randomString = undefined;
+
+console.log(typeof randomString);
+
+let randomStringInTypeString = String(randomString);
+
+console.log(typeof randomStringInTypeString);
+console.log(randomStringInTypeString);
