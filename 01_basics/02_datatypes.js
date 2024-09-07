a9700425820d42e3a45b98713311caf11c41b776
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-09-07 14:48:11 +0000

    learn datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // Treat all JS code as newer version
+
+// alert(3 + 3); // In node js runtime the alert function is not available but in browser
+
+console.log("Israfil");
+
+/*
+console.log(
+    3 
+    + 3
+) // this will not raise an error but code readablity is poor.
+
+*/
+
+let name = "Israfil"; // string
+let age = 18; // number
+let isLoggedIn = false; // boolean
+let state;
+
+// number -> rage is from -(2^53 -1) up to (2^53 - 1)
+// bigint
+// string -> ""
+// boolean -> true / false
+// null -> a standalone value
+// undefined 
+// symbol -> unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
