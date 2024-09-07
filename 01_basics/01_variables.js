19d9d76be2a84b5fd672092af09b47b536da57d2
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-09-07 14:10:01 +0000

    learn variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 34342;
+let accountEmail = "im.israfil";
+var accountPassword = "34324";
+accountCity = "Manikganj";
+let accountState;
+
+// accountId = 42453; // not allowed
+console.log(accountId);
+
+accountEmail = "astro.israfil";
+accountPassword = "12344";
+accountCity = "Dhaka";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
