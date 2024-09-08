6de12c815eb0306905e13dc4a31db9bf54cdf417
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-09-08 16:58:02 +0000

    learn javaScript object more

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,19 @@
+const tinderUser = new Object();
+tinderUser.id = "tin1323";
+tinderUser.email = "israfil@google.com";
+tinderUser.isLoggedIn = true;
+
+console.log(tinderUser);
+
+const obj1 = {a: "Israfil", b: "Alyssa"};
+const obj2 = {c: "Karolina", d: "Andrew"};
+
+// const obj3 = Object.assign({}, obj1, obj2);
+
+const obj3 = {...obj1, ...obj2};
+
+// console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(tinderUser.hasOwnProperty("email"));
