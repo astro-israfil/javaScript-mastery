9f1ee6f741b115473f7181b143328f3b3dfc2072
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-09-10 08:23:16 +0000

    Learn object destructuring and JSON

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,3 +17,14 @@ const obj3 = {...obj1, ...obj2};
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
 console.log(tinderUser.hasOwnProperty("email"));
+
+const course = {
+    name: "JavaScript Mastery",
+    courseInstructor: "Israfil",
+    price: 999,
+};
+
+
+const {name, courseInstructor: instructor} = course;
+
+console.log(name, instructor);
