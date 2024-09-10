900c250af5dc3c39743d28be153e07df6169b102
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-09-10 19:31:48 +0000

    learn switch statement in JavaScript

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,27 @@
+const day = 7;
+
+switch (day) {
+    case 1:
+        console.log("Satureday");
+        break;
+    case 2:
+        console.log("Sunday");
+        break;
+    case 3:
+        console.log("Monday");
+        break;
+    case 4:
+        console.log("Tuesday");
+        break;
+    case 5:
+        console.log("Wednesday");
+        break;
+    case 6:
+        console.log("Thursday");
+        break;
+    case 7:
+        console.log("Friday");
+        break;
+    default:
+        console.log("Invalid day");
+}
