8f9d4f52eb69e4685a57a73a2ee64f89d516caa4
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-09-10 11:25:09 +0000

    Learn more about scope and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,55 @@ let a = 300;
 if (true) {
     let a = 10;
     const b = 20;
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one() {
+    const username = "Israfil";
+    
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+
+if (true) {
+    const username = "Israfil";
+    if (username === "Israfil") {
+        const website = " youtube";
+        // console.log(username + website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ____________________________________Hoisting__________________________________
+
+console.log(addOne(10));
+
+function addOne(num) {
+    return num + 1;
+}
+
+
+console.log(addTwo(20)); // ReferenceError: Cannot access 'addTwo' before initialization 
+
+const addTwo = function (num) {
+    return num + 2;
+}
