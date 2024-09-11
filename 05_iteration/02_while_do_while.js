fbdef68d07f1a37ca2cdf942c43d131c96eb5fec
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-09-11 07:20:37 +0000

    learn while and do while loop in javaScript

---
+++ 05_iteration/02_while_do_while.js
@@ -0,0 +1,22 @@
+
+let iterator = 0;
+while (iterator < 10) {
+    // console.log(iterator);
+    iterator++;
+}
+
+
+const friends = ["Omar", "Abdullah", "Osman", "Abu Bakkar", "Walid"];
+
+let friendIndex = 0;
+while (friendIndex < friends.length) {
+    const friend = friends[friendIndex];
+    console.log(friend);
+    friendIndex++;
+}
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
